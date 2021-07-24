a1931200aca9ce92359b207a91398cfdc51d71ae
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-07-25 03:54:38 +0900

    [CHORE] Initial Commit

---
+++ app/src/main/java/com/java/mediaheaven/presenter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.java.mediaheaven.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.java.mediaheaven.R
+import com.java.mediaheaven.base.BindingActivity
+import com.java.mediaheaven.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    
+}
